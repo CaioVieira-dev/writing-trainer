8a28aa84fd2702e0685ecc144853d891136ab486
Caio Vieira <caiocesarav0208@gmail.com>
2021-09-17 10:49:33 -0300

    feat: :sparkles: implement flip card css

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,10 +1,12 @@
 //imports
 import './styles.css';
 import { FrontFace } from '../FrontFace';
+import {BackFace} from '../BackFace';
 //card
 //wraps front and back faces
 export function Card(){
     return <div className="card">
         <FrontFace/>
+        <BackFace/>
     </div>
 }
