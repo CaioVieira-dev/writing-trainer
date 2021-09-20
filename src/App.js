81ed62a55ecdafc503816502ec65cf53439ebdc7
Caio Vieira <caiocesarav0208@gmail.com>
2021-09-20 09:27:56 -0300

    feat: :clown_face: mock history bar component
    make a history bar that will show the last training the user had done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {useState} from 'react';
 import {Card} from './components/Card'
+import {HistoryBar} from './components/HistoryBar'
 import "./App.css"
 
 function App() {
   const [isFlipped,setIsFlippped] = useState(false)
   return (
     <div id="div-app">
+      <HistoryBar />
       <div class="app-wrapper">
       <div className="response-wrapper">
       <button 
---
+++ src/components/HistoryBar/index.jsx
@@ -0,0 +1,34 @@
+//imports
+import "./styles.css"
+import mockHe from "../../assets/hiragana/he.png"
+
+function Header(){
+    return <header className="history-bar-header">
+        <h2>Ultimos treinos</h2>
+    </header>
+}
+
+function PastSnapshot(){
+    
+    return <div className="past-snapshot">
+        <img src={mockHe} alt="he"/>
+        <p>He</p>
+    </div>
+}
+function History(){
+
+    return <div className="history">
+        <PastSnapshot />
+        <PastSnapshot />
+        <PastSnapshot />
+    </div>
+}
+
+//Component that shows the past drawings in a vertical list
+export function HistoryBar(){
+
+return <section className="history-bar">
+    <Header />
+    <History />
+</section>
+}
