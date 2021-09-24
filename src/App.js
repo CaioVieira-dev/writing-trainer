f6f45123cbf7eb565b0c648497bf10763007d2a3
Caio Vieira <caiocesarav0208@gmail.com>
2021-09-24 15:11:10 -0300

    feat: :clown_face: mock character list

--- src/App.js
+++ src/App.js
@@ -5,21 +5,46 @@ import "./App.css"
 
 function App() {
   const [isFlipped,setIsFlippped] = useState(false)
+  const [mockCharacterList,setMockCharacterList]=useState([
+    {character:"a",romaji:"A"},
+    {character:"ba",romaji:"Ba"},
+    {character:"ka",romaji:"Ka"},
+    {character:"ku",romaji:"Ku"},
+    {character:"he",romaji:"He"},
+    {character:"ji-shi",romaji:"Ji"}])
+  const [index,setIndex] = useState(0)
+  function makeCard(){
+    return  <Card 
+    isFlipped={isFlipped}
+    romaji={mockCharacterList[index].romaji}
+    character={mockCharacterList[index].character}/>
+    
+  }
+
   return (
     <div id="div-app">
       <HistoryBar />
       <div className="app-wrapper">
       <div className="response-wrapper">
+        <button 
+        type="button" 
+        className="response"
+        onClick={()=>{
+          if(index>=mockCharacterList.length-1){
+            setIndex(0)
+          }else{
+            setIndex(index+1)
+          }
+        }}>
+          Proximo
+        </button>
       <button 
       type="button" 
       onClick={()=>setIsFlippped(!isFlipped)}
       className="response">Ver resposta</button>
             </div>
-      <Card 
-      isFlipped={isFlipped}
-      romaji="Tsu"
-      character="tsu"/>
-      
+          {makeCard()}
+     
             </div>
     </div>
   );
